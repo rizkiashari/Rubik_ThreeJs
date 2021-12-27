51b29e3dd95769c9698a32a71f93e6d74686c8ee
RizkiAshari <rizkiashari1026@gmail.com>
2021-12-27 11:43:40 +0700

    change lib

--- src/Relax/index.js
+++ src/Relax/index.js
@@ -14,6 +14,7 @@ let startpoint = [],
 let rotate = -1,
   direction = -1; //turn around
 
+
 let x = [],
   y = [],
   z = [],
@@ -43,9 +44,9 @@ const doInit = () => {
 
   light = initLight();
   ambient = initAmbient();
-
   scene.add(light);
   scene.add(ambient);
+  initCube();
 
   controls = new OrbitControls(camera, renderer.domElement);
 
@@ -66,6 +67,41 @@ const initAmbient = () => {
   return ambient;
 };
 
+function initCube() {
+  for (let i = 0; i < 27; i++) {
+    cube[i] = new THREE.BoxGeometry(50, 50, 50);
+  }
+
+  for (let i = 0; i < 9; i++) {
+    cube[i].getAttribute("position")[2];
+    console.log(cube[i].getAttribute("position"));
+  }
+
+  for (let i = 9; i < 18; i++) {}
+
+  for (let i = 18; i < 27; i++) {}
+
+  for (let i = 0; i < 27; i++) {
+    var material = new THREE.MeshBasicMaterial({
+      vertexColors: THREE.FaceColors,
+    });
+
+    mesh[i] = new THREE.Mesh(cube[i], material);
+    mesh[i].position.set(
+      (Math.floor(i / 9) - 1) * 53,
+      (Math.floor((i % 9) / 3) - 1) * 53,
+      (Math.floor((i % 9) % 3) - 1) * 53
+    );
+    scene.add(mesh[i]);
+    x[i] = mesh[i].position.x;
+    y[i] = mesh[i].position.y;
+    z[i] = mesh[i].position.z;
+    axisX[i] = 1;
+    axisY[i] = 2;
+    axisZ[i] = 3;
+  }
+}
+
 const doRender = () => {
   requestAnimationFrame(doRender);
   controls.update();
@@ -73,6 +109,7 @@ const doRender = () => {
 };
 
 window.onload = () => {
+  raycaster = new THREE.Raycaster();
   doInit();
   doRender();
 };
